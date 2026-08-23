e60ec1da1dee2d52a29654fee50c6e06b8bda441
Sai Tej Sunkara <saitejsunka@Sais-MacBook-Pro.local>
2026-08-23 14:40:12 -0700

    fix: rename ip range to satisfy GCP api ghost cache

--- infra/modules/database/main.tf
+++ infra/modules/database/main.tf
@@ -1,6 +1,6 @@
 # Service Networking Connection for Private IP
 resource "google_compute_global_address" "private_ip_address" {
-  name          = "google-managed-services-dbexp"
+  name          = "dbexp-private-ip"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
