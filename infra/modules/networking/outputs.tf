f7eec113c00766903d176c7e3f66591a1972846d
Sai Tej Sunkara <saitejsunka@gmail.com>
2026-08-25 20:08:03 -0700

    chore: wiped infrastructure slate clean

--- infra/modules/networking/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  value       = google_compute_network.vpc_network.id
-  description = "The ID of the VPC network"
-}
-
-output "network_name" {
-  value       = google_compute_network.vpc_network.name
-  description = "The name of the VPC network"
-}
